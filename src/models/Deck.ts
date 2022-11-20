43a941b7e820988fdf1839702c2d75558a28cf75
eub1 <ebonforte@gmail.com>
2022-11-20 15:32:41 -0300

    create Deck model

---
+++ src/models/Deck.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const BlogPost = new Schema({
+  author: ObjectId,
+  title: String,
+  body: String,
+  date: Date
+});
