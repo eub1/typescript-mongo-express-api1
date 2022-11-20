e02cf1d7582a6c42a0950fd4aa7b5e3d053ba785
eub1 <ebonforte@gmail.com>
2022-11-20 16:33:44 -0300

    use express.json + create post endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,22 @@ const PORT = 3001
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
+app.post("/decks", async(req: Request, res: Response) => {
+
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  await newDeck.save();
+
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck)
+});
 
 mongoose.connect(
   'mongodb+srv://proyect-1-user1:N8TsIATejQEMNXip@project1.b401okn.mongodb.net/?retryWrites=true&w=majority'
