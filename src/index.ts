115c58a18a49a2cd6de54df76523c4782f6dbfc2
eub1 <ebonforte@gmail.com>
2022-11-20 16:16:36 -0300

    refactor mongoose connect, create PORT var

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express, {Request, Response} from "express";
 import mongoose from 'mongoose';
 
-const app = express();
+import Deck from "./models/Deck";
 
-const db = mongoose.connect('mongodb+srv://proyect-1-user1:N8TsIATejQEMNXip@project1.b401okn.mongodb.net/?retryWrites=true&w=majority');
+const PORT = 3001
+
+const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
-app.listen(3001);
+
+mongoose.connect(
+  'mongodb+srv://proyect-1-user1:N8TsIATejQEMNXip@project1.b401okn.mongodb.net/?retryWrites=true&w=majority'
+).then(()=>{
+    console.log(`Listening on port ${PORT}`);
+    app.listen(PORT);
+})
+
 
 
