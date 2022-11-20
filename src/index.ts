c987de922ed2d225df0d9f5c98cb8e055c4ecc74
eub1 <ebonforte@gmail.com>
2022-11-20 15:32:29 -0300

    connect server-mongo-mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, {Request, Response} from "express";
+import mongoose from 'mongoose';
+
 const app = express();
+
+const db = mongoose.connect('mongodb+srv://proyect-1-user1:N8TsIATejQEMNXip@project1.b401okn.mongodb.net/?retryWrites=true&w=majority');
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
