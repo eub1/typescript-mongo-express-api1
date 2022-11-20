84aa3befc6a2817ee5bf0d8ee144f88f1cf8f9e9
eub1 <ebonforte@gmail.com>
2022-11-20 16:08:34 -0300

    fix Object.Id

--- src/models/Deck.ts
+++ src/models/Deck.ts
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const ObjectId = mongoose.Types.ObjectId;
 
-const BlogPost = new Schema({
-  author: ObjectId,
+const Deck = new Schema({
   title: String,
-  body: String,
-  date: Date
 });
