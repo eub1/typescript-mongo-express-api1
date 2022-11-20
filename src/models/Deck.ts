3cc36dc6df540a239777878a33fb3da0a6c78c21
eub1 <ebonforte@gmail.com>
2022-11-20 16:12:05 -0300

    complete and export DeckModel

--- src/models/Deck.ts
+++ src/models/Deck.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const ObjectId = mongoose.Types.ObjectId;
+// const ObjectId = mongoose.Types.ObjectId;
 
-const Deck = new Schema({
+const DeckSchema = new Schema({
   title: String,
 });
+
+const DeckModel = mongoose.model('Deck', DeckSchema);
+
+
+export default DeckModel;
